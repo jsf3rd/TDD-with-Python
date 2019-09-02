b6ca79520fa20a48ec1e1bbe74dd3470366bfc9a
jsf3rd <jsf3rd@nate.com>
2019-09-02 17:42:38 +0900

    4장. 템플릿을 이용하도록 홈페이 뷰를 리펙터링

--- functional_test.py
+++ functional_test.py
@@ -1,10 +1,9 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
-
-
 import unittest
 
-class NewVisitorest(unittest.TestCase):
+class NewVisitorTest(unittest.TestCase):
     
     def setUp(self):
         binary = FirefoxBinary('C:/Program Files/Mozilla Firefox/firefox.exe')
@@ -18,6 +17,24 @@ class NewVisitorest(unittest.TestCase):
         self.browser.get('http://localhost:8000')
         
         self.assertIn('To-do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-do', header_text)
+        
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+                inputbox.get_attribute('placeholder'),
+                '작업 아이템 입력'
+        )
+        
+        inputbox.send_keys('공작깃털 사기')
+        inputbox.send_keys(Keys.Enter)
+        
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: 공작깃털 사기' for row in rows),
+        )
+        
         self.fail('Finish the test!')
 
 if __name__ == '__main__' :
