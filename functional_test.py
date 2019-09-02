0809d69eafbef155805868a1ae3cffb947caf2d6
jsf3rd <jsf3rd@nate.com>
2019-09-02 13:16:32 +0900

    최소 HTML을 반환하는 기본 뷰

--- functional_test.py
+++ functional_test.py
@@ -1,10 +1,24 @@
 from selenium import webdriver
-#from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
-#binary = FirefoxBinary('C:/Program Files/Mozilla Firefox/firefox.exe')
+from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 
-#driver = webdriver.Firefox(firefox_binary=binary)
 
-browser = webdriver.Chrome()
-browser.get('http://localhost:8000')
+import unittest
 
-assert 'Django' in browser.title
+class NewVisitorest(unittest.TestCase):
+    
+    def setUp(self):
+        binary = FirefoxBinary('C:/Program Files/Mozilla Firefox/firefox.exe')
+        self.browser = webdriver.Firefox(firefox_binary=binary)
+        self.browser.implicitly_wait(3)
+        
+    def tearDown(self):
+        self.browser.quit()
+        
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        self.browser.get('http://localhost:8000')
+        
+        self.assertIn('To-do', self.browser.title)
+        self.fail('Finish the test!')
+
+if __name__ == '__main__' :
+    unittest.main(warnings='ignore')
