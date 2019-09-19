690da3fbd0b4362c74a386c6ae2abd081a78a72b
jsf3rd <jsf3rd@nate.com>
2019-09-19 17:56:43 +0900

    레이아웃과 스타일링을 위한 FT 첫 단계

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -23,6 +23,29 @@ class NewVisitorTest(LiveServerTestCase):
         rows = table.find_elements_by_tag_name('tr')
         self.assertIn(row_text, [row.text for row in rows])
 
+    def test_layout_and_styling(self):
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+        
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )
+        
+        inputbox.send_keys('testing')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+        
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )
+
+
     def add_new_item(self, item_name):
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys(item_name)
