79768bac997ea65e9e8ba2fd6e2b1844a271d4d2
jsf3rd <jsf3rd@nate.com>
2019-09-19 13:15:04 +0900

    기존 리스트에 아이템을 추가하기 위한 신규URL 및 뷰. FT성공^^

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -6,10 +6,14 @@ import time
 
 class NewVisitorTest(LiveServerTestCase):
     
-    def setUp(self):
+    def exec_firefox(self):
         binary = FirefoxBinary('C:/Program Files/Mozilla Firefox/firefox.exe')
         self.browser = webdriver.Firefox(firefox_binary=binary)
-        self.browser.implicitly_wait(10)
+        self.browser.implicitly_wait(1)
+        
+    
+    def setUp(self):
+        self.exec_firefox()
         
     def tearDown(self):
         self.browser.quit()
@@ -18,33 +22,50 @@ class NewVisitorTest(LiveServerTestCase):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertIn(row_text, [row.text for row in rows])
-        
+
+    def add_new_item(self, item_name):
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys(item_name)
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         print(self.live_server_url)
     
         self.browser.get(self.live_server_url)
         
         header_text = self.browser.find_element_by_tag_name('h1').text
-        self.assertIn('To-Do', header_text)
+        self.assertIn('작업', header_text)
         
-        inputbox = self.browser.find_element_by_id('id_new_item')
-        self.assertEqual(
-                inputbox.get_attribute('placeholder'),
-                '작업 아이템 입력'
-        )
+        self.add_new_item('공작깃털 사기')
         
-        inputbox.send_keys('공작깃털 사기')
-        inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
-        
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, '/lists/.+')
         self.check_for_row_in_list_table('1: 공작깃털 사기')
 
-        inputbox = self.browser.find_element_by_id('id_new_item')
-        inputbox.send_keys('공작깃털을 이용해서 그물만들기')
-        inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
+        self.add_new_item('공작깃털을 이용해서 그물만들기')
         
         self.check_for_row_in_list_table('2: 공작깃털을 이용해서 그물만들기')
         self.check_for_row_in_list_table('1: 공작깃털 사기')
         
-        self.fail('Finish the test!')
+        self.browser.quit()
+        self.exec_firefox()
+        
+        print(self.live_server_url)
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('공작깃털 사기', page_text);
+        self.assertNotIn('그물만들기', page_text);
+        
+        self.add_new_item('우유 사기')
+        
+        print(self.browser.current_url)
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url, '/lists/.+')
+        self.assertNotEqual(francis_list_url, edith_list_url)
+        
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('공작깃털 사기', page_text);
+        self.assertNotIn('그물만들기', page_text);
+        self.assertIn('우유 사기', page_text);
+        
--- lists/tests.py
+++ lists/tests.py
@@ -3,26 +3,35 @@ from django.core.urlresolvers import resolve
 from django.test import TestCase
 from django.http import HttpRequest
 from lists.views import home_page
-from lists.models import Item
+from lists.models import Item, List
 
-class ItemModelTest(TestCase):
+class ListAndItemModelTest(TestCase):
+    
+    def add_item(self, list_, item_name):
+        my_item = Item()
+        my_item.list = list_
+        my_item.text = item_name
+        my_item.save()
     
     def test_saving_and_retrieving_items(self):
-        first_item = Item()
-        first_item.text = '첫 번째 아이템'
-        first_item.save()
+        list_ = List.objects.create()
             
-        second_item = Item()
-        second_item.text = '두 번째 아이템'
-        second_item.save()
+        self.add_item(list_, '첫 번째 아이템')
+        self.add_item(list_, '두 번째 아이템')
+
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list, list_)
         
         saved_items = Item.objects.all()
         self.assertEqual(saved_items.count(), 2)
         
         first_saved_item = saved_items[0]
-        second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, '첫 번째 아이템')
+        self.assertEqual(first_saved_item.list, list_)
+
+        second_saved_item = saved_items[1]
         self.assertEqual(second_saved_item.text, '두 번째 아이템')
+        self.assertEqual(second_saved_item.list, list_)
 
 class HomePageTest(TestCase):
     
@@ -36,40 +45,81 @@ class HomePageTest(TestCase):
         expected_html = render_to_string('home.html')
         self.assertEqual(response.content.decode(), expected_html)
         
-    def test_home_page_can_save_a_POST_request(self):
-        request = HttpRequest()
-        request.method = 'POST'
-        request.POST['item_text'] = '신규 작업 아이템'
+class ListViewTest(TestCase):
+
+    def test_uses_list_template(self):
+        list_ = List.objects.create()
+        response = self.client.get('/lists/%d/' % (list_.id,))
+        self.assertTemplateUsed(response, 'list.html')
+    
+    def test_displays_only_items_for_that_list(self):
+        correct_list = List.objects.create()
+        Item.objects.create(text='itemey1', list=correct_list)
+        Item.objects.create(text='itemey2', list=correct_list)
         
-        response = home_page(request)
+        other_list = List.objects.create()
+        Item.objects.create(text='other_itemey1', list=other_list)
+        Item.objects.create(text='other_itemey2', list=other_list)
+        
+        response = self.client.get('/lists/%d/' % (correct_list.id,))
+
+        self.assertContains(response, 'itemey1')
+        self.assertContains(response, 'itemey2')
+        self.assertNotContains(response, 'other_itemey1')
+        self.assertNotContains(response, 'other_itemey2')
+        
+    def test_passes_correct_list_to_template(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+        response = self.client.get('/lists/%d/' % (correct_list.id,))
+        self.assertEqual(response.context['list'], correct_list)
+        
+        
+class NewListTest(TestCase):
+
+    def test_saving_a_POST_request(self):
+        self.client.post(
+            '/lists/new',
+            data={'item_text': '신규 작업 아이템'}
+        )
         
         self.assertEqual(Item.objects.count(),1)
         new_item = Item.objects.first()
         self.assertEqual(new_item.text, '신규 작업 아이템')
  
-    def test_home_page_redirects_after_POST(self):
-        request = HttpRequest()
-        request.method = 'POST'
-        request.POST['item_text'] = '신규 작업 아이템'
-
-        response = home_page(request)
-
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/')
-
-    def test_home_page_only_saves_items_when_necessary(self):
-        request = HttpRequest()
-        home_page(request)
-        self.assertEqual(Item.objects.count(),0)
+    def test_redirects_after_POST(self):
+        response = self.client.post(
+            '/lists/new',
+            data={'item_text': '신규 작업 아이템'}
+        )
         
-    def test_home_page_displays_all_list_items(self):
-        Item.objects.create(text='itemey1')
-        Item.objects.create(text='itemey2')
+        new_list = List.objects.first()
+        self.assertRedirects(response, '/lists/%d/' % (new_list.id,))
         
-        request = HttpRequest()
-        response = home_page(request)
-
-        self.assertIn('itemey1', response.content.decode())
-        self.assertIn('itemey2', response.content.decode())
+class NewItemTest(TestCase):
+    
+    def test_can_save_a_POST_request_to_an_existing_list(self):
+        other_list =  List.objects.create()
+        correct_list = List.objects.create()
         
+        self.client.post(
+            '/lists/%d/add_item' % (correct_list.id,),
+            data={'item_text': '기존 목록에 신규 아이템'},
+        )
+        
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, '기존 목록에 신규 아이템')
+        self.assertEqual(new_item.list, correct_list)
+        
+    def test_redirects_to_list_view(self):
+        other_list =  List.objects.create()
+        correct_list = List.objects.create()
+        
+        response = self.client.post(
+            '/lists/%d/add_item' % (correct_list.id,),
+            data={'item_text': '기존 목록에 신규 아이템'},
+        )
+        
+        self.assertRedirects(response, '/lists/%d/' % (correct_list.id,))
         
