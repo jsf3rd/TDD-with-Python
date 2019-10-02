f7564c34d49055f984c6c2f995f53e692f3e9927
jsf3rd <jsf3rd@nate.com>
2019-10-02 21:43:48 +0900

    리스트 모델에 get_absolute_url을 저굥ㅇ해서 뷰에 있는 URL을 DRY함

--- lists/urls.py
+++ lists/urls.py
@@ -4,5 +4,4 @@ from django.contrib import admin
 urlpatterns = patterns('',
     url(r'^(\d+)/$','lists.views.view_list', name='view_list'),
     url(r'^new$', 'lists.views.new_list', name='new_list'),
-    # url(r'^admin/', include(admin.site.urls)),
 )
