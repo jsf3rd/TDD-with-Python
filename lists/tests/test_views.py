e636bc1563d4d61ba66526311d224de85e002d4b
jsf3rd <jsf3rd@nate.com>
2019-10-02 20:00:44 +0900

    단위 테스트를 두 개 파일로 나누기

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -5,33 +5,6 @@ from django.http import HttpRequest
 from lists.views import home_page
 from lists.models import Item, List
 
-class ListAndItemModelTest(TestCase):
-    
-    def add_item(self, list_, item_name):
-        my_item = Item()
-        my_item.list = list_
-        my_item.text = item_name
-        my_item.save()
-    
-    def test_saving_and_retrieving_items(self):
-        list_ = List.objects.create()
-            
-        self.add_item(list_, '첫 번째 아이템')
-        self.add_item(list_, '두 번째 아이템')
-
-        saved_list = List.objects.first()
-        self.assertEqual(saved_list, list_)
-        
-        saved_items = Item.objects.all()
-        self.assertEqual(saved_items.count(), 2)
-        
-        first_saved_item = saved_items[0]
-        self.assertEqual(first_saved_item.text, '첫 번째 아이템')
-        self.assertEqual(first_saved_item.list, list_)
-
-        second_saved_item = saved_items[1]
-        self.assertEqual(second_saved_item.text, '두 번째 아이템')
-        self.assertEqual(second_saved_item.list, list_)
 
 class HomePageTest(TestCase):
     
