32003f893a23ba2235d308a644bf4125f940190d
jsf3rd <jsf3rd@nate.com>
2019-10-22 17:03:45 +0900

    wait_for 적용

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -1,10 +1,12 @@
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
+from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 from selenium.webdriver.common.keys import Keys
 import sys
 import time
 
+MAX_WAIT = 10
 
 class FunctionalTest(StaticLiveServerTestCase):
     @classmethod
@@ -34,13 +36,31 @@ class FunctionalTest(StaticLiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
         
+    def wait_for(self, fn):
+        start_time = time.time()
+        while True:
+            try:
+                return fn()
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)       
+
+    def get_item_input_box(self):
+        return self.wait_for(lambda: self.browser.find_element_by_id('id_text'))
+
     def check_for_row_in_list_table(self, row_text):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertIn(row_text, [row.text for row in rows])    
+
+        
+    def wait_for_row_in_list_table(self, row_text):
+        self.wait_for(lambda: self.check_for_row_in_list_table(row_text))
         
     def add_new_item(self, item_name):
-        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox = self.get_item_input_box()
         inputbox.send_keys(item_name)
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
+        
+        
